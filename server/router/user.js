624170ca2eb92dc1db8abab2e6b9b91c54c98709
ramdayalmunda <ramdayal.munda@agamitechnologies.cin>
2023-08-11 14:09:40 +0530

    change 1

--- server/router/user.js
+++ server/router/user.js
@@ -7,7 +7,7 @@ router.post("", userController.createNewUser) // adding 1 user
 router.post("/sign-in", userController.signIn)
 router.get("/test-user", (req,res)=>{
     try{
-        res.status(200).send({"message": "Working correctly"})
+        res.status(200).send({"message": "Working correctly try 1"})
     }catch(err){
         res.status(500).send({"message": "Error"})
     }
